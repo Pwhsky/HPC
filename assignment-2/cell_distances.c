5dfb7ca0fb7b2e70436a9eb32ff053cbbd21b8de
alexl <guslechal@student.gu.se>
2023-08-19 11:26:33 +0200

    assignment 2 unfinished, segfault :(

--- assignment-2/cell_distances.c
+++ assignment-2/cell_distances.c
@@ -1,74 +1,157 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
 #include <omp.h>
 #include <math.h>
+
+
+
+
 //Program cannot use more than 5Mb of memory, i.e 5*1024*1024
-//Maximal theoretical rows = (5*1024*1024)/(bytes of a certain datatype)
+//Maximal theoretical entries = (5*1024*1024)/(bytes of a certain datatype)
 
 
 const int nDistances = 3465;
+static size_t count[3465]; //The number of unique distances
+const int blockSize = 425;
 
-double getDistance(double x, double y, double z) {
-	double output = roundf(sqrtf(x*x + y*y + z*z));
-	return output;
+static inline void getDistances(float xVal,float yVal, float zVal, float *x, float* y, float* z){
+    extern  size_t count[];
+    float innerProduct;
+    int   distance;
+    
+	for(int  i = 0; i<(blockSize);i++){
+	            innerProduct = sqrtf((xVal-x[i])*(xVal-x[i]) +
+	            			 (yVal-y[i])*(yVal-y[i]) +
+	            			 (zVal-z[i])*(zVal-z[i]));
+	        
+	            distance = (int)(100*sqrtf(innerProduct));
+	            count[distance] += 1;
+        //add innerproduct to list of distances
+	    	
+	}
+
 }
 
+
+
 int main (int argc, char** argv) {
-	//parse arguments
-	int threads = 2;
-	if (argc<2){
-	printf("No arguments given, default threads = 2\n");
-	}else {
-		threads = *(*(argv + 1)+2);
-	}
-	omp_set_num_threads(threads);
+clock_t begin = clock();
+
+
+//parse arguments
+	size_t threads = 2;
+	char arg[10];
+	for (size_t i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+    	  		memset(arg, 0, sizeof(arg));
+     		 	memcpy(arg, &argv[i][2], strlen(argv[i]) - 2);
+     		 	
+   	  		if (argv[i][1] == 't') {
+   	     			threads = (size_t) strtol(arg, NULL, 10);
+   	   		}
+
+   		 }	
+  	}
+
 	
-	
-	//open file:
-		char cwd[PATH_MAX];
-		char filename[] = "/cells";
-		getcwd(cwd,sizeof(cwd));
-		strcat(cwd,filename);
-		FILE *file = fopen(cwd,"r");
+
+//open file:
+		//char cwd[PATH_MAX];
+		//char filename[] = "/cells";
+		//getcwd(cwd,sizeof(cwd));
+		//strcat(cwd,filename);
+		
+		FILE *file = fopen("cells","r");
 		if (file == NULL){
 			printf("Error: failed to open file \n");
 			return 1;
 		}
-	//Parse contents of file: 10 will be the size of a block
-		double *x = (double*)malloc(sizeof(double)*10);
-		double *y = (double*)malloc(sizeof(double)*10);
-		double *z = (double*)malloc(sizeof(double)*10);
-		double i,j,k;
-		int index = 0;
-		while (fscanf(file, "%lf %lf %lf", &i,&j,&k) == 3){
-			x[index] = i;
-			y[index] = j;
-			z[index] = k;
-			index++;
-		}
-		float innerProduct;
-		int printval;
-		for(int i = 0; i<(10-1);i++){
-			for(int j = i+1; j< 10; j++){
+		
+//24 characters per row (including \n), compute how many rows.
+
+		 fseek(file, 0L, SEEK_END);
+    	         long int  nChars = ftell(file); 
+  		 int    nRows   = nChars/24; 
+		 rewind(file);
+	
+		int maxChunkSize = 425;      
+		int nChunks 	 = nRows/(maxChunkSize)+1; //number of chunks per thread
+		
+		
+		//THREAD INSTRUCTION:////////////////////
+		float i,j,k; 
+		float xVal,yVal,zVal;
+		
+		int index;
+		float *x = (float*)malloc(sizeof(float)*maxChunkSize);
+		float *y = (float*)malloc(sizeof(float)*maxChunkSize);
+		float *z = (float*)malloc(sizeof(float)*maxChunkSize);
+
+		for (int point = 0; point < nRows; point++) {
+			//Get point in list to compute distances from
 			
+    			 if (fscanf(file, "%f %f %f", &xVal, &yVal, &zVal) != 3) {
+  				return 1;
+			  }
+			omp_set_num_threads(threads);
+  		#pragma omp parallel for 
+  		
+      			for (int iChunk = 0; iChunk < nChunks; iChunk++){
+				//Iterate through chunks using xVal yVal zVal as point of comparison,
+				//No more than 425 floats per array!
+				
+				//place pointer at correct position in the list based on the chunk
+				
+				fseek(file, ( point*nChunks + iChunk)*3*sizeof(float)*maxChunkSize,SEEK_SET );
 			
-			innerProduct = sqrtf((x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j]) + (z[i]-z[j])*(z[i]-z[j]));
-			printf("%f\n",innerProduct);
-			
-			}	
-			
+				for (int i = 0; i < maxChunkSize; i++) {
+            				fscanf(file, "%f %f %f", &x[i], &y[i], &z[i]);
+           			}
+				
+       				getDistances(xVal, yVal, zVal, x, y, z);
+       			}
+       		
+       			
+       			 
+			//Move filepointer to the next point to compare (fscanf moves it forward)
+			fseek(file,  3 * (sizeof(float) + 1)*point ,SEEK_SET );
 			
 		}
 
-	//an entry is 7 chars
 
+		
+		
+		
+			//Compare xVal yVal zVal to all other points in the dataset by loading in chunks of 425 floats untill the end of the file.
+			
+			//dataset needs to be loaded in blocks starting at the second row, then 3th etc 
+
+		free(x);
+		free(y);
+		free(z);
+		////////////////////////////////////////
+		
+		
 	//Close file
-	free(x);
-	free(y);
-	free(z);
 	fclose(file);
+
+    //Print results:
+    for(size_t i = 0; i<nDistances; i++){
+    
+    	printf("%05.2f %ld\n", (double)i/100,count[i]);
+    }
+
+	clock_t end = clock();
+   	double elapsed_time = (double)(end - begin) / CLOCKS_PER_SEC;
+	
+	printf("Elapsed time: %lf \n",elapsed_time);
 	return 0;
 }
+
+
+
+
