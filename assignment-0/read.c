70a38ebc1ff9d48a3103c5853ec962beba2f0c9c
Alex <guslechal@student.gu.se>
2023-08-30 07:58:01 +0200

    now works for printargs

--- assignment-0/read.c
+++ assignment-0/read.c
@@ -4,31 +4,24 @@
 int main () {
 
 	FILE *file;
-	file = fopen("/home/alex/Desktop/HPC/assignment-0/matrix.txt","r");
+	file = fopen("/home/alex/Desktop/HPC/assignment-0/matrix","rb");
 	int size= sizeof(*file);
-
+	int* readData = (int*)malloc(sizeof(int)*100);
+	
+	
 	if(file == NULL) {
 		printf("error opening file \n");
 		return 1;	
 	}
 	
-
+	fread(readData, sizeof(int), 100, file);
 
     //Parse contents of file:
 	
-	int number;
-	int row = 0;
-	int col = 0;
+
 	
-	while (fscanf(file, "%d", &number) == 1){
-		if (number == col*row){
-			printf("true\n");
-		}
-		col++;
-		if (col % 10 == 0) {
-			row++;
-			col = 0;
-		}	
+	for(int i = 0; i < 100; i++){
+		printf("%d\n",readData[i]);
 	}
 	fclose(file);
 	
--- assignment-0/write.c
+++ assignment-0/write.c
@@ -3,18 +3,25 @@
 
 int main() {
 	FILE *file;
-	file = fopen("/home/alex/Desktop/HPC/assignment-0/matrix.txt","w");
+	file = fopen("/home/alex/Desktop/HPC/assignment-0/matrix","wb");
 	int size = 10;
+	
+	int*   matrixEntries = (int*)malloc(sizeof(int)*size*size);
+	int** matrix	     = (int**)malloc(sizeof(int*)*size);
+	for (size_t ix = 0, jx = 0; ix < size; ++ix, jx+=size){
+		matrix[ix] = matrixEntries + jx;
+	}
 	
 	for (int ix = 0; ix<size; ix++){
 		for (int jx = 0; jx<size;jx++){
-			int entry = ix*jx;
-			fprintf(file, "%d " ,entry);
-		
+			matrix[ix][jx] = ix*jx;
 		}	
-		fprintf(file, "\n");
 	}
 	
+	printf("made matrix \n");
+ 	fwrite(matrixEntries, sizeof(int), 100, file);
+	
+
 	fclose(file);
 	return 0;
 }
